7e28c6a53320ddac1bf3240b88c6042f71eecf69
TelariL <artem.didenko2018@gmail.com>
2024-11-05 20:46:39 +0300

    Add files via upload

---
+++ Lab11/Lab11/HeapLib/Class1.cs
@@ -0,0 +1,117 @@
+﻿using MyArrayList_Class;
+
+namespace HeapLib
+{
+    public class Heap<T> where T : IComparable<T>
+    {
+        private int size;
+        private MyArrayList<T> heap = new MyArrayList<T>(10);
+
+        public Heap(T[] array)
+        {
+            size = array.Length;
+            for (int i = 0; i < size; i++)
+            {
+                heap.Add(array[i]);
+            }
+
+            for (int i = (size / 2) - 1; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public void Heapify(int index)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int parent = index;
+
+            if (left < size && heap.Get(left).CompareTo(heap.Get(parent)) > 0)
+            {
+                parent = left;
+            }
+
+            if (right < size && heap.Get(right).CompareTo(heap.Get(parent)) > 0)
+            {
+                parent = right;
+            }
+
+            if (parent != index)
+            {
+                Swap(index, parent);
+                Heapify(parent);
+            }
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            T temp1 = heap.Get(index1);
+            T temp2 = heap.Get(index2);
+            heap.Set(index2, temp1);
+            heap.Set(index1, temp2);
+        }
+
+        public T Search()
+        {
+            return heap.Get(0);
+        }
+
+        public T Extract()
+        {
+            T exElement = Search();
+            heap.Remove(0);
+            size--;
+            Heapify(0);
+            return exElement;
+        }
+
+        public void IncreaseKey(int index, T newKey)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException("Index");
+            }
+
+            heap.Set(index, newKey);
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public void AddElement(T element)
+        {
+            heap.Add(element);
+            size++;
+
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public void MergeHeaps(Heap<T> newHeap)
+        {
+            while (newHeap.size > 0)
+            {
+                T element = newHeap.Search();
+                AddElement(element);
+            }
+
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(heap.Get(i));
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,231 @@
+﻿using HeapLib;
+using MyArrayList_Class;
+namespace Lab11
+{
+    public class MyPriorityQueue<T> where T : IComparable<T>
+    {
+        private MyArrayList<T> queue; // Внутренняя очередь с приоритетами
+        private int size; // Количество элементов в очереди с приоритетами
+        private Comparer<T> comparator; // Компаратор для сравнения элементов в очереди с приоритетами
+        // Создание пустой очереди с приоритетами с начальной ёмкостью 11
+        public MyPriorityQueue()
+        {
+            queue = new MyArrayList<T>(11);
+            size = 0;
+            comparator = Comparer<T>.Default; // Инициализация компаратора (добавляем его и в последующих конструкторах)
+        }
+        // Восстановление свойства очереди
+        public void Queuelify(int index)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int parent = index;
+
+            if (left < size && comparator.Compare(queue.Get(left), queue.Get(parent)) > 0)
+            {
+                parent = left;
+            }
+
+            if (right < size && comparator.Compare(queue.Get(right), queue.Get(parent)) > 0)
+            {
+                parent = right;
+            }
+
+            if (parent != index)
+            {
+                Swap(index, parent);
+                Queuelify(parent);
+            }
+        }
+        // Обмен элементов в очереди
+        public void Swap(int index1, int index2)
+        {
+            T temp1 = queue.Get(index1);
+            T temp2 = queue.Get(index2);
+            queue.Set(index2, temp1);
+            queue.Set(index1, temp2);
+        }
+        // Cоздание очереди с приоритетами, содержащей элементы передаваемого массива a
+        public MyPriorityQueue(T[] a)
+        {
+            queue = new MyArrayList<T>(a.Length);
+            size = a.Length;
+            comparator = Comparer<T>.Default;
+            for (int i = 0; i < size; i++)
+            {
+                queue.Add(a[i]);
+            }
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        // Cозданиe пустой очереди с приоритетами с указанной начальной ёмкостью
+        public MyPriorityQueue(int initialCcapacity)
+        {
+            queue = new MyArrayList<T>(initialCcapacity);
+            size = 0;
+            comparator = Comparer<T>.Default;
+        }
+        // Создание пустой очереди с приоритетами с указанной начальной ёмкостью и компаратором
+        public MyPriorityQueue(int initialCcapacity, Comparer<T> comparator)
+        {
+            queue = new MyArrayList<T>(initialCcapacity);
+            size = initialCcapacity;
+            this.comparator = comparator;
+        }
+        // Создание очереди с приоритетами, содержащей элементы указанной очереди с приоритетами
+        public MyPriorityQueue(MyPriorityQueue<T> c)
+        {
+            queue = new MyArrayList<T>(c.size);
+            size = c.size;
+            comparator = c.comparator;
+            for (int i = 0; i < c.size; i++)
+            {
+                queue.Add(c.queue.Get(i));
+            }
+            size = c.size;
+
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public void Add(T e)
+        {
+            queue.Add(e);
+            size++;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public void AddAll(T[] a)
+        {
+            queue.AddAll(a);
+            size += a.Length;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public void Clear()
+        {
+            queue.Clear();
+            size = 0;
+        }
+        public bool Contains(object o)
+        {
+            return queue.Contains(o);
+        }
+        public bool ContainsAll(T[] a)
+        {
+            return queue.ContainsAll(a);
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.isEmpty();
+        }
+        public void Remove(object o)
+        {
+            queue.Remove(o);
+            size--;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public void RemoveAll(T[] a)
+        {
+            queue.RemoveAll(a);
+            size -= a.Length;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public void RetainAll(T[] a)
+        {
+            queue.RetainAll(a);
+            size = a.Length;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+        }
+        public int Size()
+        {
+            return queue.Size();
+        }
+        public T[] ToArray()
+        {
+            return queue.ToArray();
+        }
+        public T[] ToArray(T[] a)
+        {
+            return queue.ToArray(a);
+        }
+        public T Element()
+        {
+            return queue.Get(0);
+        }
+        // Добавление элемента obj в очередь с приоритетами. Возвращает true, если obj добавлен, и false в противном случае
+        public bool Offer(T obj)
+        {
+            queue.Add(obj);
+            size++;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+
+            bool f = true;
+            if (queue.Contains(obj)) return f;
+            else return f == false;
+        }
+        // Возврат элемента из головы очереди с приоритетами без его удаления. Возвращает null, если очередь пуста
+        public T Peek()
+        {
+            if (size == 0) throw new ArgumentOutOfRangeException();
+            else return queue.Get(0);
+        }
+        // Удаление и возврат элемента из головы очереди с приоритетами. Возвращает null, если очередь пуста
+        public T Poll()
+        {
+            if (size == 0) throw new ArgumentOutOfRangeException();
+            else
+            {
+                T element = queue.Get(0);
+                queue.Remove(element);
+                size--;
+                for (int i = size / 2; i >= 0; i--)
+                {
+                    Queuelify(i);
+                }
+                return element;
+            }
+        }
+        // Вывод очереди с приоритетами
+        public void Print()
+        {
+            for(int i = 0; i < size; i++)
+                Console.WriteLine(queue.Get(i));
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 7, 5, 10, 23, 15, 9 };
+            MyPriorityQueue<int> queue = new MyPriorityQueue<int>(array);
+            queue.Print();
+            Console.WriteLine(queue.Offer(30));
+            queue.Print();
+            Console.WriteLine(queue.Poll());
+            Console.WriteLine();
+            queue.Print();
+        }
+    }
+}
